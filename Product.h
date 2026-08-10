ec26a5bd795c30d846a0a55e166fbaec464bcba7
Linus Gnan <info@linus-gnan.de>
2026-08-10 03:58:13 +0200

    restructuring For user interface

--- Product.h
+++ Product.h
@@ -1,10 +1,13 @@
 #ifndef PRODUCT_H
 #define PRODUCT_H
 
+#include <string>
+
 // Product is identified by its position in the products vector
 struct Product {
     int length;
     int demand;
+    std::string name;
 };
 
 #endif // PRODUCT_H
--- StockType.h
+++ StockType.h
@@ -1,12 +1,15 @@
 #ifndef STOCKTYPE_H
 #define STOCKTYPE_H
 
+#include <string>
+
 // StockType is identified by its position in the stocks vector
 struct StockType {
     int length;
     int cost;
     int availabilty; // -1 for unlimited
     int sawWidth;
+    std::string name;
 };
 
 #endif // STOCKTYPE_H
